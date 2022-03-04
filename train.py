f9a5ddfcc1344c33c816437aae7847e0cb43fa2b
harsha <harshakmohan@gmail.com>
2022-03-03 19:54:54 -0500

    tuning some hyperparameters.

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from utils import DiceLoss2D
 import os
 
 # Hyperparameters
-LEARNING_RATE = 1e-3
+LEARNING_RATE = 0.001
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 BATCH_SIZE = 4
 NUM_EPOCHS = 6
